1a82c0823f0d5ece13d12fee39a823a034cf9165
RefonteSite <lucie.phung@etu.u-paris.fr>
2025-03-24 17:08:38 +0100

    ajout fonctionnalité télécharger fichier

---
+++ js/pdf.js
@@ -0,0 +1,10 @@
+const pdf = document.querySelector('#id > button')
+
+pdf.addEventListener('click', () => {
+    const lien = document.createElement('a')
+    lien.href = "../fichiers/CV_DeveloppeuseWeb_Lucie_PHUNG_STAGE.pdf"
+    lien.download = "CV_DeveloppeuseWeb_Lucie_PHUNG_STAGE.pdf"
+    document.body.appendChild(lien)
+    lien.click()
+    document.body.removeChild(lien)
+})
