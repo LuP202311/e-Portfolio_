166ff77d5007ca98b944d16f70a70065b4b1cc19
RefonteSite <lucie.phung@etu.u-paris.fr>
2025-03-24 17:45:27 +0100

    pdf

--- js/pdf.js
+++ js/pdf.js
@@ -2,7 +2,7 @@ const pdf = document.querySelector('#cv > button')
 
 pdf.addEventListener('click', () => {
     const lien = document.createElement('a')
-    lien.href = "../fichiers/CV_DeveloppeuseWeb_Lucie_PHUNG_STAGE.pdf"
+    lien.href = "fichiers/CV_DeveloppeuseWeb_Lucie_PHUNG_STAGE.pdf"
     lien.download = "CV_DeveloppeuseWeb_Lucie_PHUNG_STAGE.pdf"
     document.body.appendChild(lien)
     lien.click()
