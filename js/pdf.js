4e5bdf9cb5ebefc77df38109b11025834023c264
lup202311 <lucie.phung@etu.u-paris.fr>
2025-05-10 20:26:15 +0200

    <update> Changement de pdf

--- js/pdf.js
+++ js/pdf.js
@@ -2,8 +2,8 @@ const pdf = document.querySelector('#cv > button')
 
 pdf.addEventListener('click', () => {
     const lien = document.createElement('a')
-    lien.href = "fichiers/CV_Informatique_Lucie_PHUNG_STAGE_BUT.pdf"
-    lien.download = "CV_Informatique_Lucie_PHUNG_STAGE_BUT.pdf"
+    lien.href = "fichiers/CV_Lucie_PHUNG_ALTERNANCE_BUT.pdf"
+    lien.download = "CV_Lucie_PHUNG_ALTERNANCE_BUT.pdf"
     document.body.appendChild(lien)
     lien.click()
     document.body.removeChild(lien)
